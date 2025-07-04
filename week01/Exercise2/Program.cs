5d412f49e521281cba1e124c49af93d48844d8e0
ElizabethGreeff <ginaoak2000@gmail.com>
2025-07-04 15:56:46 +0200

    Second assigment done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade number? ");
+        string strGrade = Console.ReadLine();
+        int intGrade = int.Parse(strGrade);
+
+        string letter = "";
+
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try again next time, you've git this!");
+        }
+
     }
 }
