00ba128e76abb7985509ce52afb8f859e85e0fe0
ElizabethGreeff <ginaoak2000@gmail.com>
2025-07-04 18:09:31 +0200

    Finished assingment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the random number game!");
+        Random randomNumber = new Random();
+        int intRand = randomNumber.Next(1, 11);
+        int guess = 0;
+
+        while (guess != intRand)
+        {
+            Console.Write("What is your guess? ");
+            string strGuess = Console.ReadLine();
+            guess = int.Parse(strGuess);
+
+
+            if (intRand > guess)
+            {
+                Console.WriteLine("Your guess is too low.");
+            }
+            else if (intRand < guess)
+            {
+                Console.WriteLine("Your guess is too high.");
+            }
+            else
+            {
+                Console.WriteLine($"You got it! The nummber was: {intRand}");
+            }
+        }
     }
 }
