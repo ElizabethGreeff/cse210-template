d304d7be644f4257371ff89a98dffbfdbd2788fa
ElizabethGreeff <ginaoak2000@gmail.com>
2025-07-04 19:12:24 +0200

    finished assignment four

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Net;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Welcome to the numbers list! To stop, enter 0.");
+        int number = 1;
+        int sum = 0;
+        int largest = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum += number;
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+        }
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
