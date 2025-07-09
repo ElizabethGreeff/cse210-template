0b9645631e78c0a08539d524bf7727363bd22e44
ElizabethGreeff <ginaoak2000@gmail.com>
2025-07-09 23:17:48 +0200

    Finished resume learning activity.

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Resume
+{
+    public string _resumeName;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DislayResume()
+    {
+        Console.WriteLine($"Name: {_resumeName}");
+        Console.WriteLine($"Jobs: ");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobInfo();
+        }
+
+    }
+
+
+
+}
