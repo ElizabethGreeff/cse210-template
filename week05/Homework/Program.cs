58569a6900b924514caa98286d133575b1d52837
ElizabethGreeff <ginaoak2000@gmail.com>
2025-07-29 20:26:59 +0200

    Finished week 5 learning acivity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Gwyneth Paltrow", "The study of iron man");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("7.3", "8-19", "Gwyneth Paltrow", "The study of iron man");
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Why we need Iron Man", "Gwyneth Paltrow", "The study of iron man");
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
